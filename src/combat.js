b4046c9ea37d3a8bb45ab41ec29dc46e6100443d
Sam Kline <sk_2013@yahoo.com>
2016-10-07 02:09:23 -0500

    Improved Combat Module

--- src/combat.js
+++ src/combat.js
@@ -1,6 +1,18 @@
+var Nonhostiles = ["Dissi"]
 var combat = {
 	IFFSafeTargetList: function(){
-	
+		var creep = this.creep;
+		var targets = _(creep.room.find(FIND_HOSTILE_CREEPS))
+						.filter(c => !Nonhostiles.includes(c.owner))
+						.value()
+		return targets
+	},
+	fireEveryThing: function(target){
+		var creep = this.creep;
+		try{creep.attack(target)}
+		try{creep.rangedAttack(target)}
+		try{creep.heal(creep)}
+		return;
 	}
 }
 module.exports = combat;
