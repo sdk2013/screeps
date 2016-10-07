79b9eb21fe9f16c6732e734c2bfdeadf275148bb
Sam Kline <sk_2013@yahoo.com>
2016-10-07 00:16:38 -0500

    implemented claimers

---
+++ src/unit_claimer.js
@@ -0,0 +1,24 @@
+var unit_claimer = {
+	beforeAge: function(){
+		var spawner = require("spawner")
+		spawner.addToQueue("claimer", {role:"claimer", task: creep.memory.task, reserveRoomName: creep.memory.reserveRoomName}, -1, false);
+	},
+	
+	behavior: function(){
+		require('tasks').runTasks(creep);
+	},
+	
+	onSpawn: function(){},
+	
+    partWeightsExt: function(extensionCount){
+        }if(extensionCount <= 18){                              // < 550 max energy avail
+            var unitWeight = null;     // cost: 500 
+        }else if(extensionCount <= 20){                              // < 800 max energy avail
+            var unitWeight = [["claim", 1],["move",2]];     // cost: 600
+        }else{                                                      // 1300 max energy
+            var unitWeight = [["claim", 2],["move",4]];     // cost: 1000
+        }
+        return unitWeight;
+    }
+}
+module.exports = unit_claimer;
