b36c0bc2570feffd922926ab2d8d53e21d91788f
Sam Kline <sk_2013@yahoo.com>
2016-10-07 02:20:54 -0500

    Combat Fixes

--- src/combat.js
+++ src/combat.js
@@ -1,14 +1,14 @@
 var Nonhostiles = ["Dissi"]
 var combat = {
 	IFFSafeTargetList: function(){
-		var creep = this.creep;
+		var creep = this;
 		var targets = _(creep.room.find(FIND_HOSTILE_CREEPS))
 						.filter(c => !Nonhostiles.includes(c.owner))
 						.value()
 		return targets
 	},
 	fireEveryThing: function(target){
-		var creep = this.creep;
+		var creep = this;
 		try{creep.attack(target)}catch(e){}
 		try{creep.rangedAttack(target)}catch(e){}
 		try{creep.heal(creep)}catch(e){}
