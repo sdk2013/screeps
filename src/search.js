cac4ed49fbeb4b78c47c67a5f39c509390872149
Sam Kline <sk_2013@yahoo.com>
2016-10-15 12:46:24 -0500

    Fixed bug causing construction to favor further roads

--- src/search.js
+++ src/search.js
@@ -74,7 +74,7 @@ module.exports = {
                         case "rampart":
                             return 30;
                         case "road":
-                            return 1 + (1 / creep.pos.getRangeTo(s));
+                            return 1 - (1 / creep.pos.getRangeTo(s));
                         default:
                             return 50;
                     }
@@ -96,7 +96,7 @@ module.exports = {
                         case "terminal":
                             return 30;
                         case "road":
-                            return 0;
+                            return 1 - (1 / creep.pos.getRangeTo(s));
                         default:
                             return 20;
                     }
