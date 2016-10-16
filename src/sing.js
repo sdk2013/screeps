ba715ae9d3e91214d46e29908f0fa309bfbb52ed
Sam Kline <sk_2013@yahoo.com>
2016-10-15 20:17:48 -0500

    Work on new song

--- src/sing.js
+++ src/sing.js
@@ -15,6 +15,11 @@ var sing = {
 			case 2:
 				status = this.houndDog(creep);
 				break;
+			/*
+			case 3:
+				status = this.somebody(creep);
+				break;
+			*/
 			default:
 				status = this.lesMis(creep);
 		}
@@ -38,14 +43,14 @@ var sing = {
 			"will water","th meadows","of france!","Can you","Hear the","people","sing?",
 			"Singing","the song","of angry","men? It is","The music","of a","people who","will not",
 			"be slaves","again!","When the","beating","of your","heart","matches","the beat-",
-			"ing of the","drums","There is a","life about","to start","when to-","morrow","comes!","", "", "", ""]
+			"ing of the","drums","There is a","life about","to start","when to-","morrow","comes!","", "", "", ""];
 	    if(Memory.songLine >= lesMisLyrics.length){
 	    	Memory.songLine = 0;
 	    	Memory.songNumber = null;
-	    	return "DONE"
+	    	return "DONE";
 	    }
 	    creep.say(lesMisLyrics[Memory.songLine], true)
-	    return "SINGING"
+	    return "SINGING";
 	},
 	houndDog: function(creep){
 		var houndDogLyrics = ["You ain't", "nothin'", "but a", "hound", "dog", "cryin' all", "the time.",
@@ -82,14 +87,37 @@ var sing = {
 						"You ain't", "nothin'", "but a", "hound", "dog", "cryin' all", "the time.",
 						"You ain't", "nothin'", "but a", "hound", "dog", "cryin' all", "the time.",
 						"Well you", "aint never", "caught a", "rabbit",
-						"And you", "aint no", "friend of", "mine!", "", "", "", ""]
+						"And you", "aint no", "friend of", "mine!", "", "", "", ""];
 		if(Memory.songLine >= houndDogLyrics.length){
 	    	Memory.songLine = 0;
 	    	Memory.songNumber = null;
-	    	return "DONE"
+	    	return "DONE";
 	    }
-	    creep.say(houndDogLyrics[Memory.songLine], true)
-	    return "SINGING"
+	    creep.say(houndDogLyrics[Memory.songLine], true);
+	    return "SINGING";
 	}
-}
+	/*,
+	somebody: function(creep){
+		var somebodyLyrics = [	"Now and", "then I", "think of", "when we", "were", "gether-",
+								"Like when", "you said", "you felt", "so happy", "you", "could die.",
+								"Told my-", "self that", "you were",  "rightforme",
+								"But felt", "so lonely", "in your", "company...",
+								"But that", "was love", "and its an", "ache I", "still re-","member.",
+								"♪♪♪♪♫♫♪",
+								"You canget", "addicted", "to a", "certain", "kind of", "sadness",
+								"Like res-", "ignation", "to the end", "always the", "end!",
+								"So when we", "found that", "we could", "not make", "sence,",
+								"Well you", "said that", "we would", "still be", "friends",
+								"But I'll", "admit that", "I was glad", "that it", "was over.",
+								"♪♪♪♪♫♫♪",];
+	    if(Memory.songLine >= lesMisLyrics.length){
+	    	Memory.songLine = 0;
+	    	Memory.songNumber = null;
+	    	return "DONE";
+	    }
+	    creep.say(somebodyLyrics[Memory.songLine], true);
+	    return "SINGING";
+	}
+	*/
+};
 module.exports = sing;
