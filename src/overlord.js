882cd1d4d0a3dc82d99c630fdf7820388dfb6c32
Sam Kline <sk_2013@yahoo.com>
2016-10-18 07:07:55 -0500

    Hotfix for debugging

--- src/overlord.js
+++ src/overlord.js
@@ -520,6 +520,7 @@ function roomIsUnderAttack(roomName){
 	var nearbyRooms = Game.map.describeExits(roomName);
 	for (const key of Object.keys(nearbyRooms)) {
 	    const nearbyRoom = nearbyRooms[key];
+	    console.log(nearbyRoom);
 	    if((!nearbyRoom.controller || nearbyRoom.controller.my) && (combat.IFFSafeTargetList(nearbyRoom, false).length !== 0)) {
 	    	return true;
 	    }
