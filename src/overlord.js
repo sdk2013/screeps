3c884d1510292b45f1e9591e7f980c58f1c188d5
Sam Kline <sk_2013@yahoo.com>
2016-10-18 10:06:52 -0500

    H

--- src/overlord.js
+++ src/overlord.js
@@ -142,6 +142,7 @@ var overlord = {
 		var old = Memory.overlord[roomName];
 		var links = old.links;
 		for(var id in links){
+			console.log(id);
 			var link = Game.getObjectById(id);
 			console.log(link);
 			//	Can't send or recieve means the link hasn't been configured
